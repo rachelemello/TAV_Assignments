6c24251d1e82e9c693d1d2ed804bf818377cf325
Sergiu <sergiu.ghita@gmail.com>
2016-02-05 13:56:56 +0200

    Added back build_bitstream method and it's delimiters declarations

--- TAV Assignment1/src/ArduinoCommunication.java
+++ TAV Assignment1/src/ArduinoCommunication.java
@@ -1,6 +1,14 @@
 
 public class ArduinoCommunication {
 	private String buffer;
+	
+	// Delimiter values for build_bitstream (with torque, udistance, infradistance)
+	private final String start_del = "001010100111001100101010"; // *s*
+	private final String tval_del = "001010100111010000101010"; // *t*
+	private final String uval_del = "001010100111010100101010"; // *u*
+	private final String ival_del = "001010100110100100101010"; // *i*
+	private final String end_del = "001010100110010100101010"; // *e*
+	
 	private String sDel = "001010100111001100101010";
 	private String eDel = "001010100110010100101010";
 	private String vDel = "001010100111011000101010";
@@ -89,6 +97,24 @@ public class ArduinoCommunication {
 		return result;							// (negativeN & positiveNTooBig & positiveNInRange)
 	}
 	
-	
+	public String build_bitstream(int t, int u, int i){
+		if(t<0 || t>255){
+			return "Error: Bad T";
+		}
+		if(u<0 || u>255){
+			return "Error: Bad U";
+		}
+		if(i<0 || i>255){
+			return "Error: Bad I";
+		}
+		
+		String ts = String.format("%8s", Integer.toBinaryString(t)).replace(' ', '0');
+		String us = String.format("%8s", Integer.toBinaryString(u)).replace(' ', '0');
+		String is = String.format("%8s", Integer.toBinaryString(i)).replace(' ', '0');
+		
+		String bitstream = start_del + tval_del + ts + uval_del + us + ival_del + is + end_del;
+		
+		return bitstream;
+	}
 
 }
