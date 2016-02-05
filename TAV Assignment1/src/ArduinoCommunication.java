6c1533c24c61b187d50b04d840793e83c22d2dc0
Sergiu <sergiu.ghita@gmail.com>
2016-02-05 14:20:21 +0200

    Merged common delimiter variables and updated additional ones to keep format
    Updated build_bitstream method to make use of updated variable names.

--- TAV Assignment1/src/ArduinoCommunication.java
+++ TAV Assignment1/src/ArduinoCommunication.java
@@ -2,17 +2,13 @@
 public class ArduinoCommunication {
 	private String buffer;
 	
-	// Delimiter values for build_bitstream (with torque, udistance, infradistance)
-	private final String start_del = "001010100111001100101010"; // *s*
-	private final String tval_del = "001010100111010000101010"; // *t*
-	private final String uval_del = "001010100111010100101010"; // *u*
-	private final String ival_del = "001010100110100100101010"; // *i*
-	private final String end_del = "001010100110010100101010"; // *e*
-	
-	private String sDel = "001010100111001100101010";
-	private String eDel = "001010100110010100101010";
-	private String vDel = "001010100111011000101010";
-	private String tDel = "001010100111010000101010";
+	// Delimiter values for bitstream packs
+	private final String sDel = "001010100111001100101010"; // *s*
+	private final String eDel = "001010100110010100101010"; // *e* 
+	private final String vDel = "001010100111011000101010"; // *v*
+	private final String tDel = "001010100111010000101010"; // *t*
+	private final String uDel = "001010100111010100101010"; // *u*
+	private final String iDel = "001010100110100100101010"; // *i*
 	
 	//Constructor: sets the buffer to empty
 	public ArduinoCommunication(){
@@ -112,7 +108,7 @@ public class ArduinoCommunication {
 		String us = String.format("%8s", Integer.toBinaryString(u)).replace(' ', '0');
 		String is = String.format("%8s", Integer.toBinaryString(i)).replace(' ', '0');
 		
-		String bitstream = start_del + tval_del + ts + uval_del + us + ival_del + is + end_del;
+		String bitstream = sDel + tDel + ts + uDel + us + iDel + is + eDel;
 		
 		return bitstream;
 	}
