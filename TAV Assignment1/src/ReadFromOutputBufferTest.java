8017f400f8209a82ce1bfc08a45f2b5ed977b0e7
Tobbe <tobsson@gmail.com>
2016-02-05 12:42:44 +0100

    Changed junit method from assertEquals to assertArrayEquals

--- TAV Assignment1/src/ReadFromOutputBufferTest.java
+++ TAV Assignment1/src/ReadFromOutputBufferTest.java
@@ -4,28 +4,28 @@ import org.junit.Test;
 
 public class ReadFromOutputBufferTest {
 	
-	ArduinoCommunication2 arduino = new ArduinoCommunication2();
+	ArduinoCommunication arduino = new ArduinoCommunication();
 	
 	@Before
-	public void setBuffer(){
-		arduino.outputBuffer = "randomstringfortestingstuff";
+	public void setOutputBuffer(){
+		arduino.setBuffer("randomstringfortestingstuff");
 	}
 	@Test
 	public void negativeN(){
 		String[] expected = {"8", ""};
 		String[] actual = arduino.readFromOutputBuffer(-1);
-		assertEquals(expected, actual);
+		assertArrayEquals(expected, actual);
 	}
 	@Test
 	public void positiveNTooBig(){
 		String[] expected = {"8", ""};
 		String[] actual = arduino.readFromOutputBuffer(50);
-		assertEquals(expected, actual);
+		assertArrayEquals(expected, actual);
 	}
 	@Test
 	public void positiveNInRange(){
 		String[] expected = {"0", "rando"};
 		String[] actual = arduino.readFromOutputBuffer(5);
-		assertEquals(expected, actual);
+		assertArrayEquals(expected, actual);
 	}
 }
