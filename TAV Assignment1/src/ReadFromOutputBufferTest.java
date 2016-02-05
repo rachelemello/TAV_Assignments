9d566696c89e5693fc1ad07602c686c19a2e5719
Tobbe <tobsson@gmail.com>
2016-02-05 12:22:58 +0100

    Changed a few lines in the test to enable @Before to be used.

--- TAV Assignment1/src/ReadFromOutputBufferTest.java
+++ TAV Assignment1/src/ReadFromOutputBufferTest.java
@@ -3,25 +3,27 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ReadFromOutputBufferTest {
-
+	
+	ArduinoCommunication2 arduino = new ArduinoCommunication2();
+	
+	@Before
+	public void setBuffer(){
+		arduino.outputBuffer = "randomstringfortestingstuff";
+	}
 	@Test
 	public void negativeN(){
-		ArduinoCommunication2 arduino = new ArduinoCommunication2();
 		String[] expected = {"8", ""};
 		String[] actual = arduino.readFromOutputBuffer(-1);
 		assertEquals(expected, actual);
 	}
 	@Test
 	public void positiveNTooBig(){
-		ArduinoCommunication2 arduino = new ArduinoCommunication2();
 		String[] expected = {"8", ""};
 		String[] actual = arduino.readFromOutputBuffer(50);
 		assertEquals(expected, actual);
 	}
 	@Test
 	public void positiveNInRange(){
-		ArduinoCommunication2 arduino = new ArduinoCommunication2();
-		arduino.outputBuffer = "randomstringfortestingstuff";
 		String[] expected = {"0", "rando"};
 		String[] actual = arduino.readFromOutputBuffer(5);
 		assertEquals(expected, actual);
